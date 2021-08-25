eeebc071918a23ef112c69926d8246de97aa65ac
emkyu404 <contact.buiminhquan@gmail.com>
2021-08-25 13:39:20 +0200

    Fin TP1

--- TP1/ActiveMQTemplate-master/src/main/java/sender/MySender.java
+++ TP1/ActiveMQTemplate-master/src/main/java/sender/MySender.java
@@ -1,6 +1,10 @@
 package sender;
 
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
 import javax.jms.Message;
+import javax.jms.MessageProducer;
 import javax.jms.Queue;
 import javax.jms.QueueConnection;
 import javax.jms.QueueConnectionFactory;
@@ -10,6 +14,7 @@ import javax.jms.Session;
 import javax.jms.TextMessage;
 import javax.jms.QueueConnectionFactory;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -25,7 +30,17 @@ public class MySender {
 			Queue queue = (Queue) applicationContext.getBean("queue");
 
 			// Create a connection. See https://docs.oracle.com/javaee/7/api/javax/jms/package-summary.html
+			ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+			Connection connection = connectionFactory.createConnection();
+			connection.start();
+			
 			// Open a session without transaction and acknowledge automatic
+			Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			
+			Destination destination = session.createTopic("DTopic");
+			
+			MessageProducer producer = session.createProducer(destination);
+			
 			// Start the connection
 			// Create a sender
 			// Create a message
