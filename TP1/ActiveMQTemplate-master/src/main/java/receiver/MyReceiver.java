dd93403df2b60338337318e934167783073c51f3
Antochg <56629942+Antochg@users.noreply.github.com>
2021-08-31 10:48:32 +0200

    receiver

--- TP1/ActiveMQTemplate-master/src/main/java/receiver/MyReceiver.java
+++ TP1/ActiveMQTemplate-master/src/main/java/receiver/MyReceiver.java
@@ -1,17 +1,18 @@
 package receiver;
 
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
 import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
 import javax.jms.Queue;
-import javax.jms.QueueConnection;
 import javax.jms.QueueConnectionFactory;
-import javax.jms.QueueReceiver;
-import javax.jms.QueueSender;
-import javax.jms.QueueSession;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 
-import javax.jms.Queue;
-import javax.jms.QueueConnectionFactory;
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -26,10 +27,36 @@ public class MyReceiver {
 			Queue queue = (Queue) applicationContext.getBean("queue");
 			
 			// Create a connection. See https://docs.oracle.com/javaee/7/api/javax/jms/package-summary.html
+			ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://192.168.43.8:61616");
+			Connection connection = connectionFactory.createConnection();
+			
 			// Open a session
+			Session session = connection.createSession(false,Session.AUTO_ACKNOWLEDGE);
+			Destination destination = session.createTopic("DTopic");
+			
 			// start the connection
+			connection.start();
+			
 			// Create a receive
+			MessageConsumer consumer = session.createConsumer(destination);
+			
 			// Receive the message
+			consumer.setMessageListener((MessageListener) new MessageListener() {
+				public void onMessage(Message message) {
+					try {
+						if(message instanceof TextMessage) {
+							TextMessage textMessage = (TextMessage) message;
+							String text = textMessage.getText();
+							System.out.println("Received : " + text);
+						} else {
+							System.out.println("Received : " + message);
+						}
+					}
+					catch(JMSException e) {
+						e.printStackTrace();
+					}
+				}
+			});
 
 		}catch(Exception e){
 			e.printStackTrace();
